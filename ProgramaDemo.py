92736fbaaeb184e53ab3ff9444eaf10c56522566
jberinguesa <jordi.beringues@gmail.com>
2024-04-07 23:24:10 +0200

    Simplificat el programa

--- ProgramaDemo.py
+++ ProgramaDemo.py
@@ -1,16 +1,10 @@
 import plotly.graph_objects as go
 import random
 
-# Generate 20 random numbers
+# Generate 20 números aleatoris
 random_numbers = [random.randint(1, 100) for _ in range(20)]
 
-# Create a table
-table = go.Figure(data=[go.Table(header=dict(values=['Numbers']),
-                 cells=dict(values=[random_numbers]))])
-
-table.show()
-
-# Create a line graph
+# Crea una línia de gràfic amb els números aleatoris
 line_graph = go.Figure(data=go.Scatter(x=list(range(1, 21)), y=random_numbers, mode='lines+markers'))
 
 line_graph.show()
